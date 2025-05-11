d651fc559d30f735e2bf8af534e6f7cfc181bee5
Henri Willman <henri.t.willman@gmail.com>
2025-05-11 14:30:28 +0300

    new build

---
+++ app/bin/main/data/tilesheets/pokemon_online_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pokemon_online_tilesheet" tilewidth="8" tileheight="8" tilecount="3844" columns="62">
+ <image source="pokemon_online_tilesheet.png" width="500" height="500"/>
+</tileset>
